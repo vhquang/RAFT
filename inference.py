8d5dab2234c7f7975bf8ffc9e06cb0868b786062
Quang Vu <vhquang@gmail.com>
2024-10-05 10:34:16 -0700

    Fix bug of dividing intput by 255

--- inference.py
+++ inference.py
@@ -25,21 +25,24 @@ def get_cpu_model(model):
         new_model[new_name] = model[name]
     return new_model
 
-def vizualize_flow(img, flo, save, counter):
-    # convert CWH to WHC format and change device if necessary  
-    img = img[0].permute(1, 2, 0).cpu().numpy()
-    flo = flo[0].permute(1, 2, 0).cpu().numpy()
- 
+def vizualize_flow(img: torch.Tensor, flo, save, counter):
+    # convert CHW (used by model) to HWC (used by cv2) format and change device if necessary  
+    img_hwc = img[0].permute(1, 2, 0).cpu().numpy()
+    flo_hwc = flo[0].permute(1, 2, 0).cpu().numpy()
+
     # map flow to rgb image
-    flo = flow_viz.flow_to_image(flo)
-    flo = cv2.cvtColor(flo, cv2.COLOR_RGB2BGR)
+    flo_img = flow_viz.flow_to_image(flo_hwc)
+    flo_img = cv2.cvtColor(flo_img, cv2.COLOR_RGB2BGR)
  
-    # concatenate, save and show images
-    img_flo = np.concatenate([img, flo], axis=0)
     if save:
-        cv2.imwrite(f"demo_frames/infer_frame_{str(counter)}.jpg", img_flo)
-    
-    cv2.imshow("Optical Flow", img_flo / 255.0)
+        # concatenate, save and show images
+        concat = np.concatenate([img_hwc, flo_img], axis=0)
+        outfile = f"output/infer_frame_{str(counter)}.jpg"
+        cv2.imwrite(outfile, concat)
+        # cv2.imshow("Optical Flow", flo_img / 255.0)
+        print(f"Image saved to {outfile}")
+    else:
+        cv2.imshow("Optical Flow", flo_img / 255.0)
  
 
 def main():
@@ -47,7 +50,7 @@ def main():
     parser.add_argument('--model', help="restore checkpoint")
     # parser.add_argument('--dataset', help="dataset for evaluation")
     parser.add_argument('--video', help="path to the video file")
-    # parser.add_argument('--small', action='store_true', help='use small model')
+    parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     # parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
     args = parser.parse_args()
@@ -58,8 +61,8 @@ def main():
     model.cuda()
     model.eval()
 
-    if torch.cuda.is_available():
-        device = "cuda"
+    # if torch.cuda.is_available():
+    #     device = "cuda"
     #     # parallel between available GPUs
     #     model = torch.nn.DataParallel(model)
     #     # load the pretrained weights into model
@@ -74,37 +77,33 @@ def main():
     #     model.load_state_dict(pretrained_weights)
 
     def preprocess(frame: np.ndarray) -> torch.Tensor:
-        # img = torch.from_numpy(img).permute(2, 0, 1).float().cuda()
-        # return img
-        
         # frame = cv2.resize(frame, (1024, 512))  # Adjust dimensions as needed
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        t_frame = torch.from_numpy(frame).permute(2, 0, 1).float() / 255.0
+        # permute to change from HWC to CHW format.
+        t_frame = torch.from_numpy(frame).permute(2, 0, 1).float()
         t_frame = t_frame.unsqueeze(0).cuda()  # Add batch dimension
         return t_frame
 
     with torch.no_grad():
-        counter = 0
+        counter = 1
         cap = cv2.VideoCapture(args.video)
         frame_1 = None
         
         while True:
-            ret, frame_2 = cap.read()
-            # frame_2 = torch.from_numpy(frame_2).permute(2, 0, 1).float().cuda()
-            frame_2 = preprocess(frame_2)
+            ret, raw_frame = cap.read()
+            
             if not ret:
                 break
-            
-            k = cv2.waitKey(0) & 0xFF
-            if k == 27:
+            if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
+            
+            frame_2 = preprocess(raw_frame)
     
             if frame_1 is not None:
-                # preprocessing
-                # frame_2 = frame_preprocess(frame_2, device)
                 # predict the flow
                 _flow_diff, flow_up = model.module(frame_1, frame_2, iters=20, test_mode=True)
-                ret = vizualize_flow(frame_1, flow_up, save=False, counter=counter)
+                if counter %10 == 0:
+                    _ret = vizualize_flow(frame_2, flow_up, save=True, counter=counter)
             
             frame_1 = frame_2
             counter += 1
